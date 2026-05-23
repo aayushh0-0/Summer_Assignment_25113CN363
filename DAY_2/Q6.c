3c4aa5369ae692c2311c61b2419c6eb24418bb1d
Ayush Rawat <ayushrawat24070@gmail.com>
2026-05-23 22:49:25 +0530

    Added DAY_2 work

---
+++ DAY_2/Q6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    int rev=0;
+    printf("Enter a Number to Reverse\n");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        int temp=n%10;
+        rev =rev+(temp*10);
+        n=n/10;
+    }
+    printf("The Reverse is ~ ",rev);
+}
