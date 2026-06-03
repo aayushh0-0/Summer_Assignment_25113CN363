bbaaa152f32c4d10cd1ffc88a9c19a9829c60d61
Ayush Rawat <ayushrawat24070@gmail.com>
2026-06-04 02:10:51 +0530

    DAY_9 Uploaded

---
+++ DAY_9/Q35.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the Number of Rows\n";
+    cin>>n;
+    for(int i=65;i<(65+n);i++)
+    {
+        for(int j=65;j<=i;j++)
+        {
+            cout<<(char)i<<" ";
+        }
+        cout<<endl;
+    }
+}
