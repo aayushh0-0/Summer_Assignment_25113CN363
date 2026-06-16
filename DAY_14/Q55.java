16a557228bf5c2bf5516b4705105e2f45712cf5f
Ayush Rawat <ayushrawat24070@gmail.com>
2026-06-16 23:00:57 +0530

    DAY_15 Uploaded

--- DAY_14/Q55.java
+++ DAY_14/Q55.java
@@ -1,36 +1,38 @@
 import java.util.*;
-public class Q55
-{
-    public static void main(String args[])
-    {
+
+public class Q55 {
+    public static void main(String args[]) {
+
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter Size of Array");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
+
         int a[] = new int[n];
+
         System.out.println("Enter the Elements of Array");
-        for(int i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
+
         System.out.println("Array is ~ ");
-        for(int i=0;i<n;i++)
-        {
-            System.out.print(a[i]+" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
         }
-        int lar= a[0];
-        int seclar=a[0];
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]>lar)
-            {
-                seclar=a[i];
-                if(seclar<lar)
-                {
-                    lar=seclar;
-                    seclar=a[i];
-                }
+
+        int lar = a[0];
+        int seclar = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n; i++) {
+
+            if (a[i] > lar) {
+                seclar = lar;
+                lar = a[i];
+            } else if (a[i] > seclar && a[i] != lar) {
+                seclar = a[i];
             }
         }
-        System.out.println("\nSecond Largest Element is ~ "+seclar);
+        System.out.println("Second Largest Element is ~ " + seclar);
+        sc.close();
     }
 }
--- DAY_14/Q56.java
+++ DAY_14/Q56.java
@@ -1,32 +1,28 @@
 import java.util.*;
-public class Q56
-{
-    public static void main(String args[])
-    {
+
+public class Q56 {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of Array");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         int a[] = new int[n];
-        HashMap<Integer,Integer> map = new HashMap<>();
-        boolean dupcheck=false;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        boolean dupcheck = false;
         System.out.println("Enter the Elements in Array");
-        for(int i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
-            map.put(a[i],map.getOrDefault(a[i],0)+1);
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
         }
         System.out.println("Duplicates are ~");
-        for(Map.Entry<Integer,Integer> entry : map.entrySet())
-        {
-            if(entry.getValue()>1)
-            {
-                dupcheck=true;
-                System.out.print(entry.getKey()+" ");
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                dupcheck = true;
+                System.out.print(entry.getKey() + " ");
             }
         }
-        if(!dupcheck)
-        {
+        if (!dupcheck) {
             System.out.println("No Duplicates Found");
         }
+        sc.close();
     }
 }
