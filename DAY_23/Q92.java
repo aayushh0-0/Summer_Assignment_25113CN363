ae53246fda70467d9eda810f91a0763761e7fbef
Ayush Rawat <ayushrawat24070@gmail.com>
2026-06-21 23:49:07 +0530

    DAY_23 Uploaded

--- DAY_23/Q92.java
+++ DAY_23/Q92.java
@@ -4,10 +4,20 @@ public class Q92 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String");
-
-        // Remove leading, trailing and middle spaces
-        String a = sc.nextLine().trim().replace(" ", "");
-        System.out.println("String After Removing Space is ~ " + a);
-        sc.close();
+        String a=sc.nextLine().trim();
+        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+        for(char ch:a.toCharArray())
+        {
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int max=0;
+        for(int val:map.values())
+        {
+            if(val>max)
+            {
+                max=val;
+            }
+        }
+        System.out.println("Maximum Occurence is of ~ "+map.keySet(max));
     }
 }
